5d7f7e61ffa7f89373ac38b0079bba3932de1cd3
Ray Estrada <raymond.estrada@gsa.gov>
2023-02-01 13:59:23 -0800

    Add local settings and service file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -822,9 +822,10 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+if (file_exists(DRUPAL_ROOT . '/sites/settings.local.php')) {
+  include DRUPAL_ROOT . '/sites/settings.local.php';
+}
+
 $databases['default']['default'] = array (
   'database' => 'drupal10',
   'username' => 'drupal10',
