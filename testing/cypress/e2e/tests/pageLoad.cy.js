b3bf381a676bfaa5de7d0fb30f12b8297fb87eb4
Christian Medders <christian.medders@bixal.com>
2023-02-17 13:47:09 -0500

    updated test

--- testing/cypress/e2e/tests/pageLoad.cy.js
+++ testing/cypress/e2e/tests/pageLoad.cy.js
@@ -1,8 +1,11 @@
 /// <reference types="cypress" />
 
 describe('check page load', () => {
+  beforeEach('vist site', () => {
+    cy.visit('/')
+  })
+  
   it('verify page is loading', () =>{
-    cy.visit('http://html.vote-gov-d10.lndo.site/')
     cy.get('[class="layout-container"]').should('be.visible')
   })
 })
