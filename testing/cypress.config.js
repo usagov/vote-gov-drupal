3f1650a7712f57a5624a616263dbe50fd15f436e
Christian Medders <christian.medders@bixal.com>
2023-02-22 13:36:39 -0500

    added envs for log in

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -15,6 +15,21 @@ reporter: 'cypress-mochawesome-reporter',
     responsetimeout: 10000,
     "blockHosts": ["www.google-analytics.com", "ssl.google-analytics.com"],
 
+    env: {
+      // info for site admin role
+      "site_admin": "admin_test",
+      "admin_password": "vote123",
+      "admin_role": "site_admin",
+      // info for content editor role
+      "content_editor": "editor_test",
+      "editor_password": "vote1234",
+      "editor_role": "content_editor",
+      // info for content mananger role 
+      "content_manager": "manager_test",
+      "manager_password": "vote123",
+      "manager_role": "content_manager",
+    },
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
