56c4a332c67afbf8b926ccd190836c86a94abe71
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-02-24 15:16:22 -0500

    clean up cloudgov settings commented out code.

--- web/sites/default/settings.cloudgov.php
+++ web/sites/default/settings.cloudgov.php
@@ -3,7 +3,7 @@
 /**
  * @file
  * Settings file for cloud.gov.
- */
+**/
 
 $cf_application_data = json_decode(getenv('VCAP_APPLICATION') ?? '{}', TRUE);
 $cf_service_data = json_decode(getenv('VCAP_SERVICES') ?? '{}', TRUE);
@@ -31,31 +31,7 @@ if (getenv('NEW_RELIC_API_KEY')) {
   $config['new_relic_rpm.settings']['api_key'] = getenv('NEW_RELIC_API_KEY');
 }
 
-/**
- * Collect external service information from environment.
- * Cloud Foundry places all service credentials in VCAP_SERVICES
-**/
-// $server_http_host = $_SERVER['HTTP_HOST'];
-// if (!empty($cf_application_data['space_name']) &&
-//     in_array($cf_application_data['space_name'],
-//     ['dev', 'stage', 'prod'])) {
-//   switch (strtolower($cf_application_data['space_name'])) {
-//     case "dev":
-//       $server_http_host = 'https://cms-dev.vote.gov';
-//       break;
-//     case "stage":
-//       $server_http_host = 'https://cms-stage.vote.gov';
-//       break;
-//     case "prod":
-//       $server_http_host = 'https://cms.vote.gov';
-//       break;
-//    }
-// }
-
-
 $is_cloudgov=FALSE;
-
-// $server_http_host = $_SERVER['HTTP_HOST'] ?? '';
 
 if (!empty($cf_application_data['space_name']) &&
     $application_environment != 'local') {
@@ -93,13 +69,6 @@ foreach ($cf_service_data as $service_list) {
         'driver' => 'mysql',
         'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
       ];
-
-      // if ( $is_cloudgov===TRUE ) {
-      //   $databases['default']['default']['pdo'] = [
-      //     \PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/rds-combined-ca-us-gov-bundle.pem',
-      //     \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => TRUE
-      //   ];
-      // }
     } else if ( stristr($service['name'], 'secrets') ) {
       $settings['hash_salt'] = $service['credentials']['HASH_SALT'];
     } else if ( stristr($service['name'], 'storage') ) {
@@ -107,24 +76,16 @@ foreach ($cf_service_data as $service_list) {
       $settings['s3fs.secret_key'] = $service['credentials']['secret_access_key'];
       $config['s3fs.settings']['bucket'] = $service['credentials']['bucket'];
       $config['s3fs.settings']['region'] = $service['credentials']['region'];
-
       $config['s3fs.settings']['disable_cert_verify'] = FALSE;
-
       $config['s3fs.settings']['root_folder'] = '';
       $config['s3fs.settings']['public_folder'] = 'sites/default/files';
       $config['s3fs.settings']['private_folder'] = 'private';
-
-      // $config['s3fs.settings']['use_cname'] = TRUE;
-      // $config['s3fs.settings']['domain'] = $_SERVER['HTTP_HOST'] . '/sites/default/files';
       $config['s3fs.settings']['domain_root'] = 'public';
-
       $config['s3fs.settings']['use_customhost'] = FALSE;
       $config['s3fs.settings']['hostname'] = $service['credentials']['fips_endpoint'];
       $config['s3fs.settings']['use-path-style-endpoint'] = FALSE;
-
       $config['s3fs.settings']['use_cssjs_host'] = FALSE;
       $config['s3fs.settings']['cssjs_host'] = '';
-
       $config['s3fs.settings']['use_https'] = TRUE;
       $settings['s3fs.upload_as_private'] = FALSE;
       $settings['s3fs.use_s3_for_public'] = TRUE;
@@ -134,35 +95,5 @@ foreach ($cf_service_data as $service_list) {
 }
 
 $settings['php_storage']['twig']['directory'] = '../storage/php';
-
-// CSS and JS aggregation need per dyno/container cache.
-// This is from https://www.fomfus.com/articles/how-to-create-a-drupal-8-project-for-heroku-part-1
-// included here without fully understanding implications:
 $settings['cache']['bins']['data'] = 'cache.backend.php';
-
 $settings['trusted_host_patterns'][] = $applicaiton_fqdn_regex;
-// var_dump($settings['trusted_host_patterns']);
-
-// if (!empty($application_environment)) {
-//   switch (strtolower($applicaiton_environment)) {
-//     case "local":
-//       $settings['trusted_host_patterns'][] = '^cms-local.usa.gov$';
-//       $settings['trusted_host_patterns'][] = '^cms-local-usagov.apps.internal$';
-//       break;
-// 
-//     case "dev":
-//       $settings['trusted_host_patterns'][] = '^cms-dev.vote.gov$';
-//       $settings['trusted_host_patterns'][] = '^cms-dev-usagov.apps.internal$';
-//       break;
-// 
-//     case "stage":
-//       $settings['trusted_host_patterns'][] = '^cms-stage.vote.gov$';
-//       $settings['trusted_host_patterns'][] = '^cms-stage-usagov.apps.internal$';
-//       break;
-// 
-//     case "prod":
-//       $settings['trusted_host_patterns'][] = '^cms.vote.gov$';
-//       $settings['trusted_host_patterns'][] = '^cms-prod-usagov.apps.internal$';
-//       break;
-//   }
-// }
