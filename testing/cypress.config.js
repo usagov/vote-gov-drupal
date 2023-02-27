cc5d7df1c5170d888d1fb52d49d182a18a178009
Christian Medders <christian.medders@bixal.com>
2023-02-27 14:14:33 -0500

    updated envs

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -16,20 +16,25 @@ reporter: 'cypress-mochawesome-reporter',
     "blockHosts": ["www.google-analytics.com", "ssl.google-analytics.com"],
 
     env: {
-      // info for site admin role
-      "site_admin": "admin_test",
-      "admin_password": "vote123",
-      "admin_role": "site_admin",
-      // info for content editor role
-      "content_editor": "editor_test",
-      "editor_password": "vote1234",
-      "editor_role": "content_editor",
-      // info for content mananger role 
-      "content_manager": "manager_test",
-      "manager_password": "vote123",
-      "manager_role": "content_manager",
-    },
-
+      "test_pass": "password",
+      "roles": {
+            // info for site admin role
+            "site_admin": {
+                  "username": "admin_test",
+                  "role": "site_admin"
+            },
+            // info for content editor role
+            "content_editor": {
+                  "username": "editor_test",
+                  "role": "content_editor"
+            },
+            // info for content mananger role 
+            "content_manager": {
+                  "username": "manager_test",
+                  "role": "content_manager"
+            }
+      }
+},
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
