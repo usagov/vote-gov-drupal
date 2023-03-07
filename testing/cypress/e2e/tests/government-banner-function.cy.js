6b794ef5cd177b023b8023d8cc50cacb7b9bbe2d
Christian Medders <christian.medders@bixal.com>
2023-03-07 11:17:31 -0500

    updated test to check more access and fucntion

---
+++ testing/cypress/e2e/tests/government-banner-function.cy.js
@@ -0,0 +1,68 @@
+// <reference types="cypress" />
+
+describe('Ofical Website Function', () => {
+
+  it('check that site admin access', () => {
+    cy.signin(Cypress.env('roles').site_admin.username, Cypress.env('test_pass'))
+
+    // each role should have access to edit
+    cy.request('http://vote-gov.lndo.site/block/1').then((response) => {
+  expect(response.status).to.eq(200)
+    })
+
+    // only site admin should be able to create banner, editor and manager should not have access to url
+    cy.request('http://vote-gov.lndo.site//block/add/government_banner').then((response) => {
+      expect(response.status).to.eq(200)
+        })
+
+    cy.logout()
+  })
+
+  it('check that content editor access', () => {
+    cy.signin(Cypress.env('roles').content_editor.username, Cypress.env('test_pass'))
+
+     // each role should have access to edit
+    cy.request('http://vote-gov.lndo.site/block/1').then((response) => {
+      expect(response.status).to.eq(200)
+        }) 
+
+    // only site admin should be able to create banner, editor and manager should not have access to url
+    cy.request({
+          url: 'http://vote-gov.lndo.site/block/add/government_banner',
+          failOnStatusCode:false,
+      }).then((resp) => {
+          expect(resp.status).to.eq(403)
+      })
+
+    cy.logout()    
+  })
+
+  it('check that content manager access', () => {
+    cy.signin(Cypress.env('roles').content_manager.username, Cypress.env('test_pass'))
+
+     // each role should have access to edit
+    cy.request('http://vote-gov.lndo.site/block/1').then((response) => {
+      expect(response.status).to.eq(200)
+        })
+
+    // only site admin should be able to create banner, editor and manager should not have access to url 
+    cy.request({
+          url: 'http://vote-gov.lndo.site/block/add/government_banner',
+          failOnStatusCode:false,
+      }).then((resp) => {
+          expect(resp.status).to.eq(403)
+      })    
+
+    cy.logout()
+  })
+
+  it('check header function', () => {
+    cy.visit('/')
+
+    cy.get('[data-test="header-logo"]').should('be.visible')
+    cy.get('[data-test="header-button"]').click().get('[data-test="header-banner"]').should('be.visible')
+    // check to see if button hides banner as well
+    cy.get('[data-test="header-button"]').click().get('[data-test="header-banner"]').should('not.be.visible')
+  })
+
+})
