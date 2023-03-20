63c5a3b9b30e40b51e875e8defea968a8b6a3f1e
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-03-20 14:06:21 -0400

    [VOTE-1077] Cypress pipeline testing.

--- scripts/pipeline/cloud-gov-remote-command.sh
+++ scripts/pipeline/cloud-gov-remote-command.sh
@@ -3,11 +3,11 @@
 application=$1
 command=$2
 
-export PATH=$PATH:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush
+PATH=$PATH:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush
 
 [ -z "${application}" ] || [ -z "${command}" ] && echo "Command error! Valid format: ${0} <application_name> <command>" && exit 1
 
 echo "Running command: '$(echo ${command} | cut -d' ' -f1,2)'..."
 {
-  cf ssh ${application} -c "${command}"
+  cf ssh ${application} -c "PATH=${PATH}; ${command}"
 } >/dev/null 2>&1
