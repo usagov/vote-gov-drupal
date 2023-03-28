6a28e19920a1d9994b9a12354656032408684012
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-03-28 12:50:48 -0400

    [VOTE-952]: Merge for migration testing.

--- scripts/pipeline/deb-mysql-client-install.sh
+++ scripts/pipeline/deb-mysql-client-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 {
-    wget -U "" https://dev.mysql.com/get/mysql-apt-config_0.8.18-1_all.deb
+    wget https://dev.mysql.com/get/mysql-apt-config_0.8.18-1_all.deb
     ## To work for rootless and root images.
     if [ "$(whoami)" != "root" ]; then
       sudo dpkg -i mysql-apt-config_0.8.18-1_all.deb
