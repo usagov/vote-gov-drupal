062d38d8e97b2390a111a630c2187643314aa5a5
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-03-28 12:39:53 -0400

    [VOTE-952]: Merge for migration testing.

--- scripts/pipeline/deb-cf-install.sh
+++ scripts/pipeline/deb-cf-install.sh
@@ -3,12 +3,12 @@
 echo "Installing CloudFoundry repository..."
 {
   if [ "$(whoami)" != "root" ]; then
-    wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+    wget -U "" -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
     echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
     sudo apt-get update
     sudo apt-get install -y cf8-cli
   else
-    wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+    wget -U "" -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
     echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
     apt-get update
     apt-get install -y cf8-cli
