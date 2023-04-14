10b6be4a7e1fb000583056b68c7208a65f94af7b
mlloydbixal <amanda.lloyd@gsa.gov>
2023-04-14 14:24:06 -0400

    updating script

--- scripts/cronish.sh
+++ scripts/cronish.sh
@@ -3,7 +3,6 @@
 while true
 do
   echo "Running 'drush cron'..."
-  drush --root=$HOME/web core:cron --uri
-  echo "'drush cron' task completed!"
+  drush --root=$HOME/web core:cron --uri=https://vote-drupal-test.app.cloud.gov  echo "'drush cron' task completed!"
   sleep 60m
 done
