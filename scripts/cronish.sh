63a84eabfb4120e485acc825bdfd0e429501098a
Ray Estrada <raymond.estrada@gsa.gov>
2023-04-14 12:23:53 -0700

    Fix cron script line wrap

--- scripts/cronish.sh
+++ scripts/cronish.sh
@@ -1,8 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 while true
 do
   echo "Running 'drush cron'..."
-  drush --root=$HOME/web core:cron --uri=https://vote-drupal-test.app.cloud.gov  echo "'drush cron' task completed!"
+  drush --root=$HOME/web core:cron --uri=https://vote-drupal-test.app.cloud.gov
+  echo "'drush cron' task completed!"
   sleep 60m
 done
