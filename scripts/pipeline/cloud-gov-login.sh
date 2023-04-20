9aa819dd879d5cbbdb44000094952f7eb1c37038
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-04-20 18:06:01 -0400

    [VOTE-1335]: Update manifest and login scripts for Terraform.

--- scripts/pipeline/cloud-gov-login.sh
+++ scripts/pipeline/cloud-gov-login.sh
@@ -5,15 +5,19 @@ cf_env=$1
 if [ "${cf_env}" == "prod" ]; then
   export cf_space=${prod_cf_space}
   export drupal_memory=${prod_drupal_memory}
+  export waf_name=${prod_waf_name}
 elif [ "${cf_env}" == "stage" ]; then
   export cf_space=${stage_cf_space}
   export drupal_memory=${stage_drupal_memory}
+  export waf_name=${stage_waf_name}
 elif [ "${cf_env}" == "test" ]; then
   export cf_space=${test_cf_space}
   export drupal_memory=${test_drupal_memory}
+  export waf_name=${test_waf_name}
 elif [ "${cf_env}" == "dev" ]; then
   export cf_space=${dev_cf_space}
   [ -z "${cf_space}" ] && export cf_space=${CF_SPACE_DEV}
+  export waf_name=${dev_waf_name}
   export drupal_memory=${dev_drupal_memory}
 fi
 
