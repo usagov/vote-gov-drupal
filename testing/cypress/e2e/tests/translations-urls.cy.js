24be0a71e36608aab3507585fd10e2409572cd67
Christian Medders <christian.medders@bixal.com>
2023-05-05 13:46:16 -0400

    updated class name and array list

--- testing/cypress/e2e/tests/translations-urls.cy.js
+++ testing/cypress/e2e/tests/translations-urls.cy.js
@@ -5,29 +5,29 @@ describe('test translations', () => {
   it('check that url updates with change', () => {
     cy.visit('/about-us')
       cy.get('[data-test="language-dropdown"]').click().get('[data-test="language-selection"]').then(selection => {
-        cy.get(selection).get('[class="usa-language__submenu-item"]').then(li => {
+        cy.get(selection).get('[class="usa-language__submenu-item is-active"]').then(li => {
           // Spanish
-          cy.get(li[0]).find('a').should('have.attr', 'href').and('include', 'es')
+          cy.get(li[1]).find('a').should('have.attr', 'href').and('include', 'es')
           // Bengali
-          cy.get(li[1]).find('a').should('have.attr', 'href').and('include', 'bn')
+          cy.get(li[2]).find('a').should('have.attr', 'href').and('include', 'bn')
           // Chinese - Simplified
-          cy.get(li[2]).find('a').should('have.attr', 'href').and('include', 'zh-hans')
+          cy.get(li[3]).find('a').should('have.attr', 'href').and('include', 'zh-hans')
           // Chinese - Traditional
-          cy.get(li[3]).find('a').should('have.attr', 'href').and('include', 'zh')
+          cy.get(li[4]).find('a').should('have.attr', 'href').and('include', 'zh')
           // Hindi
-          cy.get(li[4]).find('a').should('have.attr', 'href').and('include', 'hi')
+          cy.get(li[5]).find('a').should('have.attr', 'href').and('include', 'hi')
           // Khmer
-          cy.get(li[5]).find('a').should('have.attr', 'href').and('include', 'km')
+          cy.get(li[6]).find('a').should('have.attr', 'href').and('include', 'km')
           // Korean
-          cy.get(li[6]).find('a').should('have.attr', 'href').and('include', 'ko')
+          cy.get(li[7]).find('a').should('have.attr', 'href').and('include', 'ko')
           // Navajo
-          cy.get(li[7]).find('a').should('have.attr', 'href').and('include', 'nv')
+          cy.get(li[8]).find('a').should('have.attr', 'href').and('include', 'nv')
           // Tagalog
-          cy.get(li[8]).find('a').should('have.attr', 'href').and('include', 'tl')
+          cy.get(li[9]).find('a').should('have.attr', 'href').and('include', 'tl')
           // Vietnamese
-          cy.get(li[9]).find('a').should('have.attr', 'href').and('include', 'vi')
+          cy.get(li[10]).find('a').should('have.attr', 'href').and('include', 'vi')
           // Yup'ik
-          cy.get(li[10]).find('a').should('have.attr', 'href').and('include', 'ypk')
+          cy.get(li[11]).find('a').should('have.attr', 'href').and('include', 'ypk')
           })
         })
       })
