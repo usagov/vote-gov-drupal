b5f60e8f942cc6b246d8b16c96e09691da3de74a
Christian Medders <christian.medders@bixal.com>
2023-05-08 14:23:07 -0400

    removed unused code

--- testing/cypress/support/commands.js
+++ testing/cypress/support/commands.js
@@ -72,11 +72,3 @@ Cypress.Commands.add('logout', function () {
       expect(resp.status).to.eq(302)
   });
 });
-
-
-Cypress.Commands.add("type_ckeditor", (element, content) => {
-  cy.window()
-    .then(win => {
-      win.CKEDITOR.instances[element].setData(content);
-    });
-});
