af06c3d3d5bba452b43a2dbee533e2fe2286b649
Christian Medders <christian.medders@bixal.com>
2023-05-08 12:14:20 -0400

    udpated test for cover role access

--- testing/cypress/e2e/tests/user-content-access.cy.js
+++ testing/cypress/e2e/tests/user-content-access.cy.js
@@ -5,53 +5,72 @@ describe('Test User Role Access to Content Moderation', () => {
   it('Test Site Admin', () => {
     cy.signin(Cypress.env('roles').site_admin.username, Cypress.env('test_pass'))
 
-
     // Can create a draft
     cy.visit('/node/add/page')
     cy.get('[data-drupal-selector="edit-title-0-value"]').type('Test Page')
 
-    cy.get('[class="ck-blurred ck ck-content ck-editor__editable ck-rounded-corners ck-editor__editable_inline"]').each(field => {
-      cy.get(field).type('test page')
+    cy.get('[class="ck-blurred ck ck-content ck-editor__editable ck-rounded-corners ck-editor__editable_inline"]').then(field => {
+      cy.get(field[0]).clear().realType('test page')
+      cy.get(field[1]).clear().realType('test page')
+
     })
-      
+    cy.get('[data-drupal-selector="edit-submit"]').click()
+    cy.url().should('contain', 'test-page')
+ 
 
-    // Can view any draft
-    // ? should i make a page to test this and then delete later?
+    // Can view draft content 
+    cy.visit('/admin/content')
+    cy.get('[data-drupal-selector="edit-status"]').select('Unpublished')
+    cy.get('[data-drupal-selector="edit-submit-content"]').click()
+    cy.get('[class="view-content"]').invoke('text').should('contain', 'Test Page')
 
-    // Can modify any draft
-    // ? should i make a page to test this and then delete later?
+    // Can modify draft content
+    cy.visit('/admin/content')
+    cy.get('[data-drupal-selector="edit-status"]').select('Unpublished')
+    cy.get('[data-drupal-selector="edit-submit-content"]').click()
+    cy.get('[class="edit dropbutton__item dropbutton-action"]').click()
+    cy.get('[class="page-title"]').should('contain', 'Edit Basic Page Test Page')
 
     // Can publish content
-    // *http://vote-gov.lndo.site/node/add/page
-    // todo: check that user has option for publish
+    cy.visit('/test-page-0')
+    cy.get('[data-drupal-selector="edit-new-state"]').select('Published')
+    cy.get('[data-drupal-selector="edit-submit"]').click()
+    cy.get('[class="usa-alert__body"]').should('contain', 'The moderation state has been updated.')
 
     // Can view published content
-    // ? should i make a page to test this and then delete later?
+    cy.request('/admin/content').then((response) => {
+      expect(response.status).to.eq(200)})
 
-    // Can view archived content
-    // ? should i make a page to test this and then delete later?
+    // Can move content to archived status 
+    cy.visit('/test-page-0')
+    cy.get('[class="usa-button-group__item"]').then(btn => {
+      cy.get(btn[1]).click()
+    })
+    cy.get('[data-drupal-selector="edit-moderation-state-0-state"]').select('Archived')
+    cy.get('[data-drupal-selector="edit-submit"]').click()
+    cy.get('[id="edit-current"]').should('contain', 'Archived')
 
-    // Can move archived content to draft or restore
-    // ? should i make a page to test this and then delete later?
+    // Can move archived content to draft
+    cy.visit('/test-page-0')
+    cy.get('[data-drupal-selector="edit-new-state"]').select('Draft')
+    cy.get('[data-drupal-selector="edit-submit"]').click()
+    cy.get('[id="edit-current"]').should('contain', 'Draft')
 
     // Can delete content
-    // ? should i make a page to test this and then delete later?
-    // * check with a current page if user can delete the content
+    cy.visit('/test-page-0')
+    cy.get('[class="usa-button-group__item"]').then(btn => {
+      cy.get(btn[2]).click()
+    })
+    cy.get('[data-drupal-selector="edit-submit"]').click()
+    cy.get('[class="usa-alert__body"]').should('contain', 'The Basic Page Test Page has been deleted.')
 
     // Can create/edit/delete media
-    // * http://vote-gov.lndo.site/admin/structure/media/add
-
-    // Site Admin:
-
-    // Can execute any content moderation action
-    // * http://vote-gov.lndo.site/admin/content/moderated
-
-    // draft publish archived restore
-
-    // view all
+    cy.request('/admin/structure/media/add').then((response) => {
+      expect(response.status).to.eq(200)})
 
     // Can update the content moderation settings
-    // * http://vote-gov.lndo.site/admin/people/permissions
+    cy.request('/admin/people/permissions').then((response) => {
+      expect(response.status).to.eq(200)})
   })
 
 })
