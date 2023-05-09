fe736e575d0212c2f57e86e3c61b81fe3f3960c0
mlloydbixal <amanda.lloyd@gsa.gov>
2023-05-09 12:51:03 -0400

    add node path to js

--- web/themes/custom/vote_gov/src/js/registration-tool.js
+++ web/themes/custom/vote_gov/src/js/registration-tool.js
@@ -1,15 +1,14 @@
 (function (Drupal) {
     Drupal.behaviors.registrationTool = {
-      attach: function (context, settings) {
+      attach: function () {
         document.getElementById('register').onsubmit = function (ev) {
           var stateSelected = document.getElementById('js-user-selection').value;
+          var stateRelativePath = document.getElementById(stateSelected).attributes.data.value;
           if (stateSelected === 'default') {
             return false;
           } else {
             ev.preventDefault();
-            var redirectURL = 'http://vote-gov.lndo.site/register/';
-            redirectURL = redirectURL + stateSelected + '/';
-            window.location.href = redirectURL;
+            window.location.href = stateRelativePath;
           }
         };
       },
