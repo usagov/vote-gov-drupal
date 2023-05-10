84670bd52b8d05362bbb4fc1cf418c5ae7040c4c
Christian Medders <christian.medders@bixal.com>
2023-05-10 16:13:29 -0400

    updated after ray review

--- testing/cypress/e2e/tests/user-content-access.cy.js
+++ testing/cypress/e2e/tests/user-content-access.cy.js
@@ -14,35 +14,33 @@ describe('Test User Role Access to Content Moderation', () => {
       cy.get(field[1]).clear().realType('test page')
 
     })
+    cy.get('[class="claro-details__summary claro-details__summary--accordion-item"]').then(dropdown => {
+      cy.get(dropdown[3]).click()
+      cy.get('[data-drupal-selector="edit-path-0-pathauto"]').click()
+      cy.get('[data-drupal-selector="edit-path-0-alias"]').type('/cypress-content-moderation-test')
+    })
     cy.get('[data-drupal-selector="edit-submit"]').click()
-    cy.url().should('contain', 'test-page')
- 
+    cy.url().should('contain', 'cypress-content-moderation-test')
 
     // Can view draft content 
-    cy.visit('/admin/content')
-    cy.get('[data-drupal-selector="edit-status"]').select('Unpublished')
-    cy.get('[data-drupal-selector="edit-submit-content"]').click()
-    cy.get('[class="view-content"]').invoke('text').should('contain', 'Test Page')
-
+    cy.request('/cypress-content-moderation-test').then((response) => {
+      expect(response.status).to.eq(200)})
+   
     // Can modify draft content
-    cy.visit('/admin/content')
-    cy.get('[data-drupal-selector="edit-status"]').select('Unpublished')
-    cy.get('[data-drupal-selector="edit-submit-content"]').click()
-    cy.get('[class="edit dropbutton__item dropbutton-action"]').click()
-    cy.get('[class="page-title"]').should('contain', 'Edit Basic Page Test Page')
+    cy.visit('/cypress-content-moderation-test')
+    cy.get('[class="usa-button-group__item"]').then(btn => {
+      cy.get(btn[1]).click()
+    })
+    cy.url().should('contain', 'edit')
 
     // Can publish content
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[data-drupal-selector="edit-new-state"]').select('Published')
     cy.get('[data-drupal-selector="edit-submit"]').click()
     cy.get('[class="usa-alert__body"]').should('contain', 'The moderation state has been updated.')
 
-    // Can view published content
-    cy.request('/admin/content').then((response) => {
-      expect(response.status).to.eq(200)})
-
     // Can move content to archived status 
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[class="usa-button-group__item"]').then(btn => {
       cy.get(btn[1]).click()
     })
@@ -51,13 +49,13 @@ describe('Test User Role Access to Content Moderation', () => {
     cy.get('[id="edit-current"]').should('contain', 'Archived')
 
     // Can move archived content to draft
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[data-drupal-selector="edit-new-state"]').select('Draft')
     cy.get('[data-drupal-selector="edit-submit"]').click()
     cy.get('[id="edit-current"]').should('contain', 'Draft')
 
     // Can delete content
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[class="usa-button-group__item"]').then(btn => {
       cy.get(btn[2]).click()
     })
@@ -69,13 +67,13 @@ describe('Test User Role Access to Content Moderation', () => {
       expect(response.status).to.eq(200)})
 
     // Can update the content moderation settings
-    cy.request('/admin/people/permissions').then((response) => {
+    cy.request('/admin/config/workflow/workflows/manage/publishing_content').then((response) => {
       expect(response.status).to.eq(200)})
 
       cy.logout()
   })
 
-  it('Test Content Manager', () => {
+  it.only('Test Content Manager', () => {
     cy.signin(Cypress.env('roles').content_manager.username, Cypress.env('test_pass'))
 
     // Can create a draft
@@ -87,35 +85,33 @@ describe('Test User Role Access to Content Moderation', () => {
       cy.get(field[1]).clear().realType('test page')
 
     })
+    cy.get('[class="claro-details__summary claro-details__summary--accordion-item"]').then(dropdown => {
+      cy.get(dropdown[0]).click()
+      cy.get('[data-drupal-selector="edit-menu-enabled"]').click()
+      cy.get('[data-drupal-selector="edit-menu-title"]').clear().type('/cypress-content-moderation-test')
+    })
     cy.get('[data-drupal-selector="edit-submit"]').click()
-    cy.url().should('contain', 'test-page')
- 
+    cy.url().should('contain', 'cypress-content-moderation-test')
 
     // Can view draft content 
-    cy.visit('/admin/content')
-    cy.get('[data-drupal-selector="edit-status"]').select('Unpublished')
-    cy.get('[data-drupal-selector="edit-submit-content"]').click()
-    cy.get('[class="view-content"]').invoke('text').should('contain', 'Test Page')
-
+    cy.request('/cypress-content-moderation-test').then((response) => {
+      expect(response.status).to.eq(200)})
+   
     // Can modify draft content
-    cy.visit('/admin/content')
-    cy.get('[data-drupal-selector="edit-status"]').select('Unpublished')
-    cy.get('[data-drupal-selector="edit-submit-content"]').click()
-    cy.get('[class="edit dropbutton__item dropbutton-action"]').click()
-    cy.get('[class="page-title"]').should('contain', 'Edit Basic Page Test Page')
+    cy.visit('/cypress-content-moderation-test')
+    cy.get('[class="usa-button-group__item"]').then(btn => {
+      cy.get(btn[1]).click()
+    })
+    cy.url().should('contain', 'edit')
 
     // Can publish content
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[data-drupal-selector="edit-new-state"]').select('Published')
     cy.get('[data-drupal-selector="edit-submit"]').click()
     cy.get('[class="usa-alert__body"]').should('contain', 'The moderation state has been updated.')
 
-    // Can view published content
-    cy.request('/admin/content').then((response) => {
-      expect(response.status).to.eq(200)})
-
     // Can move content to archived status 
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[class="usa-button-group__item"]').then(btn => {
       cy.get(btn[1]).click()
     })
@@ -124,13 +120,13 @@ describe('Test User Role Access to Content Moderation', () => {
     cy.get('[id="edit-current"]').should('contain', 'Archived')
 
     // Can move archived content to draft
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[data-drupal-selector="edit-new-state"]').select('Draft')
     cy.get('[data-drupal-selector="edit-submit"]').click()
     cy.get('[id="edit-current"]').should('contain', 'Draft')
 
     // Can delete content
-    cy.visit('/test-page-0')
+    cy.visit('/cypress-content-moderation-test')
     cy.get('[class="usa-button-group__item"]').then(btn => {
       cy.get(btn[2]).click()
     })
@@ -147,7 +143,7 @@ describe('Test User Role Access to Content Moderation', () => {
 
     // should not be able to update the content moderation settings
     cy.request({
-      url: '/admin/people/permissions',
+      url: '/admin/config/workflow/workflows/manage/publishing_content',
       failOnStatusCode: false,
     }).then((resp) => {
       expect(resp.status).to.eq(403)
