f68ab80823c9458156b7a593fe4c1ff5ad5e1fd5
mlloydbixal <amanda.lloyd@gsa.gov>
2023-05-10 09:59:07 -0400

    rename view and simplify options code

--- web/themes/custom/vote_gov/src/js/registration-tool.js
+++ web/themes/custom/vote_gov/src/js/registration-tool.js
@@ -3,12 +3,11 @@
       attach: function () {
         document.getElementById('register').onsubmit = function (ev) {
           var stateSelected = document.getElementById('js-user-selection').value;
-          var stateRelativePath = document.getElementById(stateSelected).attributes.data.value;
           if (stateSelected === 'default') {
             return false;
           } else {
             ev.preventDefault();
-            window.location.href = stateRelativePath;
+            window.location.href = stateSelected;
           }
         };
       },
