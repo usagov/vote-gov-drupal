3eb6e06fac46e6ce6616d851ff7ab4d8d0c89650
Ray Estrada <raymond.estrada@gsa.gov>
2023-05-19 16:24:31 -0700

    Update file name and test message

--- testing/cypress/e2e/tests/language-selector.cy.js
+++ testing/cypress/e2e/tests/language-selector.cy.js
@@ -4,9 +4,9 @@ const testPages = require("../../fixtures/language-selection.json")
 
 // * This is written to verify that each language will give the user a valid webpage.  This is done by checking the response status should be 200 and also checking that the drop down is visible when clicked
 
-describe('test translations', () => {
+describe('Test language selector', () => {
   testPages.forEach((page) => {
-  it(`check that url updates will change for ${page.name} page`, () => {
+  it(`Validate language selector links on ${page.name} page`, () => {
     cy.visit({
       url: page.route,
     })
