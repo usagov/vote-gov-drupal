558650709e5f87d5752bb0ad3ee89b4e9aa9f948
Nehemiah Abuga <nehemiah.abuga@bixal.com>
2023-05-19 10:14:37 -0700

    changes to utilize base url and feed data from fixtures

--- testing/cypress/e2e/tests/translations-urls.cy.js
+++ testing/cypress/e2e/tests/translations-urls.cy.js
@@ -1,23 +1,15 @@
 /// <reference types="cypress" />
 
-const testPages = [
-  "http://vote-gov.lndo.site/",
-  "http://vote-gov.lndo.site/register/al/",
-  "http://vote-gov.lndo.site/register/ak/",
-  "http://vote-gov.lndo.site/register/as/",
-  "http://vote-gov.lndo.site/register/ar/",
-  "http://vote-gov.lndo.site/register/nd/",
-  "http://vote-gov.lndo.site/register/wy/",
-  "http://vote-gov.lndo.site/about-us",
-  "http://vote-gov.lndo.site/404",
-]
+const testPages = require("../../fixtures/language-selection.json")
 
 // * This is written to verify that each language will give the user a valid webpage.  This is done by checking the response status should be 200 and also checking that the drop down is visible when clicked
 
 describe('test translations', () => {
   testPages.forEach((page) => {
-  it('check that url updates with change', () => {
-    cy.visit(page)
+  it(`check that url updates will change for ${page.name} page`, () => {
+    cy.visit({
+      url: page.route,
+    })
       cy.get('[data-test="language-dropdown"]').click().get('[data-test="language-selection"]').should('be.visible').then(selection => {
         cy.get(selection).get('[class="usa-language__submenu-item is-active"]').each(li => {
             cy.get(li).find('a').each(link => {
