3d887d1d710a19ea5ee5e0fa52c064d8aa49d721
casey-rapnicki-bixal <71328874+casey-rapnicki-bixal@users.noreply.github.com>
2023-05-23 13:20:16 -0400

    VOTE-1514: update deploy script dev reference to branch var (#208)

--- scripts/pipeline/cloud-gov-deploy.sh
+++ scripts/pipeline/cloud-gov-deploy.sh
@@ -5,5 +5,5 @@ envsubst < manifest.tmp > manifest.yml
 
 cf push
 cf add-network-policy ${project}-drupal-${CIRCLE_BRANCH} ${proxy_name} -s ${proxy_space} --protocol tcp --port ${proxy_port}
-cf add-network-policy ${waf_name} ${project}-drupal-${CIRCLE_BRANCH} -s "${project}-dev" --protocol tcp --port ${drupal_port}
+cf add-network-policy ${waf_name} ${project}-drupal-${CIRCLE_BRANCH} -s "${project}-${CIRCLE_BRANCH}" --protocol tcp --port ${drupal_port}
 cf run-task ${project}-drupal-${CIRCLE_BRANCH} --command "./scripts/post-deploy" --name "${project}-${CIRCLE_BRANCH}-post-deploy"  -k "2G" -m "256M"
