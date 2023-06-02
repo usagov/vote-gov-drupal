536054f21c334a22ed8e3b19250177811127494a
Christian Medders <christian.medders@bixal.com>
2023-06-02 10:22:33 -0400

    added step to check the status of the url

--- testing/cypress/e2e/tests/state-registration.cy.js
+++ testing/cypress/e2e/tests/state-registration.cy.js
@@ -11,8 +11,11 @@ describe('Validate state selection options', () => {
           cy.wrap(value)
           cy.get('[data-test="state-selection"]').select(opn)
           cy.get('[data-test="register-button"]').click()
-          // * comparing the URL to the sorted value 
-          cy.url().should('contain', value)
+          // * comparing the URL to the sorted value and then checking that the url is giving a status of 200
+          cy.url().should('contain', value).then(url => {
+            cy.request(url).then((response) => {
+              expect(response.status).to.eq(200)})
+          })
           cy.get('[data-test="back-button"]').click()
         })
       })
