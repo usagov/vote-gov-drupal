34f5fad85bc2fd983862e1fbbb5f028cc3c702c5
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-06-02 18:00:13 -0400

    VOTE-1560: add tome generation after deploy

--- scripts/pipeline/cloud-gov-deploy.sh
+++ scripts/pipeline/cloud-gov-deploy.sh
@@ -11,3 +11,4 @@ cf push
 cf add-network-policy ${project}-drupal-${branch} ${proxy_name} -s ${proxy_space} --protocol tcp --port ${proxy_port}
 cf add-network-policy ${waf_name} ${project}-drupal-${branch} -s "${project}-${branch}" --protocol tcp --port ${drupal_port}
 cf run-task ${project}-drupal-${branch} --command "./scripts/post-deploy" --name "${project}-${branch}-post-deploy"  -k "2G" -m "256M"
+cf run-task ${project}-drupal-${CIRCLE_BRANCH} --command "scripts/build_static" --name "${project}-${CIRCLE_BRANCH}-tome"  -k "2G" -m "256M"
