6a623c8feb5b39db3270c0fc16c3cf1325926dd3
Casey Rapnicki <casey.rapnicki@bixal.com>
2023-06-08 12:58:27 -0400

    VOTE-1578: fix variable

--- scripts/pipeline/cloud-gov-deploy.sh
+++ scripts/pipeline/cloud-gov-deploy.sh
@@ -5,7 +5,7 @@ wait_for_task(){
   count=0
   echo "task id: '${task_id}'"
   while : ; do
-    status=$(cf tasks ${project}-drupal-${CIRCLE_BRANCH} | awk "$1==${task_id} {print $3}")
+    status=$(cf tasks ${project}-drupal-${CIRCLE_BRANCH} | awk -v task_id="${task_id}" '$1==task_id {print $3}')
     echo "status: ${status:-NA}"
     if [ "${status}" = "SUCCEEDED" ]; then
       break
