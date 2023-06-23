1d4ed797e593ffc1644f73cfbe6cf488b52b2f03
Christian Medders <christian.medders@bixal.com>
2023-06-23 15:56:58 -0400

    added other links to be skipped

--- testing/cypress/e2e/externalLinks/english-external-links-validator.cy.js
+++ testing/cypress/e2e/externalLinks/english-external-links-validator.cy.js
@@ -35,6 +35,7 @@ const excludedlinks = [
   'https://voterlookup.ohiosos.gov/voterlookup.aspx',
   'https://vote.sos.ri.gov/Voter/RegisterToVote',
   'https://vote.sos.ri.gov/Home/UpdateVoterRecord?ActiveFlag=0',
+  'https://vivote.gov/voters/register-to-vote/',
   // sc links
   'https://vrems.scvotes.sc.gov/Voter/Login?ref=voteusa_en',
   'https://scvotes.gov/voters/register-to-vote/?ref=voteusa_en',
