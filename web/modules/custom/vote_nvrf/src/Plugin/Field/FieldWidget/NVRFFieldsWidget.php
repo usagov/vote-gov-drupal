ea53e6c2daf57a3a7be2df355908973b69fcf5d5
daniel crowder <daniel.crowder@gsa.gov>
2023-09-07 19:17:07 -0400

    VOTE-244: Create custom module, Vote NVRF, to provide compound field for state/territory content type.

---
+++ web/modules/custom/vote_nvrf/src/Plugin/Field/FieldWidget/NVRFFieldsWidget.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\vote_nvrf\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+
+/**
+ * Defines the 'field_nvrf_fields' field widget.
+ *
+ * @FieldWidget(
+ *   id = "field_nvrf_fields",
+ *   label = @Translation("NVRF Fields"),
+ *   field_types = {"field_nvrf_fields"},
+ * )
+ */
+class NVRFFieldsWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    
+    $element['nvrf_field'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('NVRF Field'),
+      '#target_type' => 'taxonomy_term',
+      '#default_value' => isset($items[$delta]->nvrf_field) ? \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($items[$delta]->nvrf_field) : NULL,
+      '#step' => 0.01,
+      '#selection_settings' => [
+        'include_anonymous' => FALSE,
+        'target_bundles' => array('nvrf_field'),
+    ],
+    ];
+
+    $element['required'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Required'),
+      '#default_value' => isset($items[$delta]->required) ? $items[$delta]->required : NULL,
+    ];
+
+    $element['#theme_wrappers'] = ['container', 'form_element'];
+    $element['#attributes']['class'][] = 'field-nvrf-fields-elements';
+    $element['#attached']['library'][] = 'vote_nvrf/field_nvrf_fields';
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function errorElement(array $element, ConstraintViolationInterface $violation, array $form, FormStateInterface $form_state) {
+    return isset($violation->arrayPropertyPath[0]) ? $element[$violation->arrayPropertyPath[0]] : $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
+    foreach ($values as $delta => $value) {
+      if ($value['nvrf_field'] === '') {
+        $values[$delta]['nvrf_field'] = NULL;
+      }
+      if ($value['required'] === '') {
+        $values[$delta]['required'] = NULL;
+      }
+    }
+    return $values;
+  }
+
+}
