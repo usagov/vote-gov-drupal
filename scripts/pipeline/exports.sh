4c6bf46d57ef42809c5a1849f7a668aea96f8e89
tt-gsa <tyler.thompson@gsa.gov>
2023-09-13 16:07:31 -0400

    VOTE-67 setting site uri used to generate sitemap.xml to always be the static site's

--- scripts/pipeline/exports.sh
+++ scripts/pipeline/exports.sh
@@ -8,6 +8,7 @@ if [ "${cf_env}" == "prod" ]; then
   export cms_uri=${prod_cms_uri}
   export drupal_instances=${prod_drupal_instances}
   export drupal_memory=${prod_drupal_memory}
+  export ssg_uri=${prod_ssg_uri}
   export sso_x509_cert=${prod_sso_x509_cert}
   export waf_name=${prod_waf_name}
   export waf_external_endpoint=${prod_waf_external_endpoint}
@@ -16,6 +17,7 @@ elif [ "${cf_env}" == "stage" ]; then
   export cms_uri=${stage_cms_uri}
   export drupal_instances=${stage_drupal_instances}
   export drupal_memory=${stage_drupal_memory}
+  export ssg_uri=${stage_ssg_uri}
   export sso_x509_cert=${stage_sso_x509_cert}
   export waf_name=${stage_waf_name}
   export waf_external_endpoint=${stage_waf_external_endpoint}
@@ -24,6 +26,7 @@ elif [ "${cf_env}" == "test" ]; then
   export cms_uri=${test_cms_uri}
   export drupal_memory=${test_drupal_memory}
   export drupal_instances=${test_drupal_instances}
+  export ssg_uri=${test_ssg_uri}
   export sso_x509_cert=${test_sso_x509_cert}
   export waf_name=${test_waf_name}
   export waf_external_endpoint=${test_waf_external_endpoint}
@@ -33,6 +36,7 @@ elif [ "${cf_env}" == "dev" ]; then
   export cms_uri=${dev_cms_uri}
   export drupal_instances=${dev_drupal_instances}
   export drupal_memory=${dev_drupal_memory}
+  export ssg_uri=${dev_ssg_uri}
   export sso_x509_cert=${dev_sso_x509_cert}
   export waf_name=${dev_waf_name}
   export waf_external_endpoint=${dev_waf_external_endpoint}
