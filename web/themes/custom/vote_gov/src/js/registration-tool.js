3a360db0e04851b9b9d2932e2790b9be2da0a346
mlloydbixal <amanda.lloyd@gsa.gov>
2023-09-21 19:13:32 -0400

    VOTE-251: replace state dropdown with link list combo box

--- web/themes/custom/vote_gov/src/js/registration-tool.js
+++ web/themes/custom/vote_gov/src/js/registration-tool.js
@@ -1,15 +1,32 @@
-(function (Drupal) {
-    Drupal.behaviors.registrationTool = {
-      attach: function () {
-        document.getElementById('register').onsubmit = function (ev) {
-          var stateSelected = document.getElementById('js-user-selection').value;
-          if (stateSelected === 'default') {
-            return false;
-          } else {
-            ev.preventDefault();
-            window.location.href = stateSelected;
+  (function (Drupal) {
+    Drupal.behaviors.nvrfPdfDownload = {
+      attach: function (context, settings) {
+        function filterStateList() {
+          var input, filter, ul, li, a, i, txtValue;
+          input = document.getElementById("state-input");
+          filter = input.value.toUpperCase();
+          ul = document.getElementById("state-list-results");
+          li = ul.getElementsByTagName("li");
+          for (i = 0; i < li.length; i++) {
+              a = li[i].getElementsByTagName("a")[0];
+              txtValue = a.textContent || a.innerText;
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                  li[i].style.display = "";
+              } else {
+                  li[i].style.display = "none";
+              }
           }
-        };
+        }
+        
+        function stateListVisible() {
+          var x = document.getElementById("filtered-states-list");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+        
       },
     };
   })(Drupal);
