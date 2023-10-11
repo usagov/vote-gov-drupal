e6619613688090a2ee47afd8498a13759e593486
daniel crowder <daniel.crowder@gsa.gov>
2023-10-11 18:25:51 -0400

    VOTE-326: Code cleanup per PHPLint

--- web/modules/custom/vote_nvrf/src/Plugin/Field/FieldFormatter/NVRFDoubleFieldsJsonFormatter.php
+++ web/modules/custom/vote_nvrf/src/Plugin/Field/FieldFormatter/NVRFDoubleFieldsJsonFormatter.php
@@ -5,6 +5,7 @@ namespace Drupal\vote_nvrf\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\rest_views\SerializedData;
+
 /**
  * Plugin implementation of the 'field_nvrf_double_fields_json' formatter.
  *
@@ -25,23 +26,24 @@ class NVRFDoubleFieldsJsonFormatter extends FormatterBase {
     $field_name = $items->getName();
 
     foreach ($items as $delta => $item) {
-        
-        $first = !empty($item->get('first')->getValue()) ? $item->get('first')->getValue() : '';
-        $second = !empty($item->get('second')->getValue()) ? $item->get('second')->getValue() : '';
 
-       $data_array = [
+      $first = !empty($item->get('first')->getValue()) ? $item->get('first')->getValue() : '';
+      $second = !empty($item->get('second')->getValue()) ? $item->get('second')->getValue() : '';
+
+      $data_array = [
         'first' => $first,
         'second' => $second,
-        ];
+      ];
 
-        $elements[$delta] = [
-            '#type' => 'data',
-            '#data' => SerializedData::create($data_array),
-          ];
-          
-        //Unset just in case
-        unset($elements[$delta]['#plain_text']);
-    } 
+      $elements[$delta] = [
+        '#type' => 'data',
+        '#data' => SerializedData::create($data_array),
+      ];
+
+      // Unset just in case.
+      unset($elements[$delta]['#plain_text']);
+    }
     return $elements;
   }
+
 }
--- web/modules/custom/vote_nvrf/src/Plugin/Field/FieldFormatter/NVRFFieldsJsonFormatter.php
+++ web/modules/custom/vote_nvrf/src/Plugin/Field/FieldFormatter/NVRFFieldsJsonFormatter.php
@@ -6,6 +6,7 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\rest_views\SerializedData;
 use Drupal\taxonomy\Entity\Term;
+
 /**
  * Plugin implementation of the 'field_nvrf_fields_json' formatter.
  *
@@ -23,31 +24,32 @@ class NVRFFieldsJsonFormatter extends FormatterBase {
   public function viewElements(FieldItemListInterface $items, $langcode): array {
     $elements = [];
     foreach ($items as $delta => $item) {
-        
-        $nvrf_field_tid = $item->get('nvrf_field')->getValue();
-        $required = $item->get('required')->getValue();
-        $term = Term::load($nvrf_field_tid);
 
-        $help_text = !empty($term->get('field_help_text')->getValue()) ? $term->get('field_help_text')->get(0)->getValue()['value'] : '';
-        $instructions = !empty($term->get('field_instructions')->getValue()) ? $term->get('field_instructions')->get(0)->getValue()['value'] : '';
-        $display_label = !empty($term->get('field_display_label')->getValue()) ? $term->get('field_display_label')->get(0)->getValue()['value'] : '';
+      $nvrf_field_tid = $item->get('nvrf_field')->getValue();
+      $required = $item->get('required')->getValue();
+      $term = Term::load($nvrf_field_tid);
 
-        $term_data_array = [
-          'name' => $term->getName(),
-          'display_label' => $display_label,
-          'help_text' => $help_text,
-          'instructions' => $instructions,
-          'required' => $required,
-        ];
+      $help_text = !empty($term->get('field_help_text')->getValue()) ? $term->get('field_help_text')->get(0)->getValue()['value'] : '';
+      $instructions = !empty($term->get('field_instructions')->getValue()) ? $term->get('field_instructions')->get(0)->getValue()['value'] : '';
+      $display_label = !empty($term->get('field_display_label')->getValue()) ? $term->get('field_display_label')->get(0)->getValue()['value'] : '';
 
-        $elements[$delta] = [
-            '#type' => 'data',
-            '#data' => SerializedData::create($term_data_array),
-          ];
-          
-        //Unset just in case  
-        unset($elements[$delta]['#plain_text']);
-    } 
+      $term_data_array = [
+        'name' => $term->getName(),
+        'display_label' => $display_label,
+        'help_text' => $help_text,
+        'instructions' => $instructions,
+        'required' => $required,
+      ];
+
+      $elements[$delta] = [
+        '#type' => 'data',
+        '#data' => SerializedData::create($term_data_array),
+      ];
+
+      // Unset just in case.
+      unset($elements[$delta]['#plain_text']);
+    }
     return $elements;
   }
+
 }
