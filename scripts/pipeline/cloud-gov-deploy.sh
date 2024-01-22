e5f533273143c7d64f6fe14f1aee216f76d0ad3d
tt-gsa <tyler.thompson@gsa.gov>
2024-01-22 08:42:30 -0500

    VOTE-712 testing

--- scripts/pipeline/cloud-gov-deploy.sh
+++ scripts/pipeline/cloud-gov-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+start=$(date +%s)
+
 wait_for_task(){
   task_id=$1
   count=0
@@ -29,12 +31,17 @@ envsubst < manifest.tmp > manifest.yml
 space=${CIRCLE_BRANCH}
 [ "${CIRCLE_BRANCH}" = "test" ] && space="dev"
 
-cf push --strategy rolling
+time cf push --strategy rolling
 
-cf add-network-policy ${project}-drupal-${CIRCLE_BRANCH} ${proxy_name} -s ${proxy_space} --protocol tcp --port ${proxy_port}
-cf add-network-policy ${waf_name} ${project}-drupal-${CIRCLE_BRANCH} -s "${project}-${space}" --protocol tcp --port ${drupal_port}
+time cf add-network-policy ${project}-drupal-${CIRCLE_BRANCH} ${proxy_name} -s ${proxy_space} --protocol tcp --port ${proxy_port}
+time cf add-network-policy ${waf_name} ${project}-drupal-${CIRCLE_BRANCH} -s "${project}-${space}" --protocol tcp --port ${drupal_port}
 echo "Running post deploy steps..."
 task_id=$(cf run-task ${project}-drupal-${CIRCLE_BRANCH} --command "./scripts/post-deploy" --name "${project}-${CIRCLE_BRANCH}-post-deploy"  -k "2G" -m "256M" | grep 'task id:' | awk '{print $NF}')
-wait_for_task ${task_id}
+time wait_for_task ${task_id}
 echo "Running static site export..."
 task_id=$(cf run-task ${project}-drupal-${CIRCLE_BRANCH} --command "./scripts/build_static" --name "${project}-${CIRCLE_BRANCH}-tome"  -k "2G" -m "256M")
+
+end=$(date +%s)
+runtime=$((end-start))
+echo $runtime | awk '{printf "%d:%02d:%02d", $1/3600, ($1/60)%60, $1%60}'
+echo $SECONDS | awk '{printf "%d:%02d:%02d", $1/3600, ($1/60)%60, $1%60}'
