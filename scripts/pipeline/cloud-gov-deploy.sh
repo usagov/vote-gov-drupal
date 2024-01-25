f5e497b0c37f10d8b351775d41c7632dc25dee70
tt-gsa <tyler.thompson@gsa.gov>
2024-01-25 10:31:21 -0500

    VOTE-712 testing

--- scripts/pipeline/cloud-gov-deploy.sh
+++ scripts/pipeline/cloud-gov-deploy.sh
@@ -12,7 +12,7 @@ time cf add-network-policy ${project}-drupal-${CIRCLE_BRANCH} ${proxy_name} -s $
 time cf add-network-policy ${waf_name} ${project}-drupal-${CIRCLE_BRANCH} -s "${project}-${space}" --protocol tcp --port ${drupal_port}
 echo "Running post deploy steps..."
 time cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/post-deploy"
-echo "Running static site export..."
-time cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/build_static"
+echo "Running upkeep..."
+time cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/upkeep"
 
 echo $SECONDS | awk '{printf "%d:%02d:%02d", $1/3600, ($1/60)%60, $1%60}'
