225a1e94c5c7d16898753ce9421d10638b50c9a5
Mandy Lloyd <121046395+mlloydbixal@users.noreply.github.com>
2024-01-29 15:33:18 -0500

    VOTE-611: develop and theme voter guide detail page (#523)

--- web/themes/custom/votegov/src/js/external-links.js
+++ web/themes/custom/votegov/src/js/external-links.js
@@ -11,11 +11,10 @@
   // Process external links.
   if (externalLinks.length) {
     for (let i = 0; i < externalLinks.length; i++) {
-      const svg = `<svg aria-hidden="true" viewBox="3 3 18 18" width="10" height="10"><path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" fill="currentColor"></path></svg>`;
-      var svgIcon = `<span class="ext-link">&#xFEFF;${svg}</span>`;
+      externalLinks[i].classList.add('usa-link--external');
       externalLinks[i].setAttribute('target', '_blank');
-      externalLinks[i].insertAdjacentHTML('beforeend', svgIcon);
       externalLinks[i].setAttribute('title', title_translated);
+      externalLinks[i].setAttribute('rel', 'noreferrer');
     }
   }
 
