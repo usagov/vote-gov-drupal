6cead66ed9f34edd1b1851347aef5439bc2c1b88
tt-gsa <tyler.thompson@gsa.gov>
2024-02-13 10:43:47 -0500

    VOTE-541: remove taxonomy pages from static site after generation

--- scripts/pipeline/cloud-gov-post-deploy-upkeep.sh
+++ scripts/pipeline/cloud-gov-post-deploy-upkeep.sh
@@ -1,2 +1,2 @@
 echo "Running upkeep..."
-cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/upkeep >/dev/null 2>&1"
+cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/upkeep >/dev/null 2>&1 && echo 'Successfully completed upkeep!' || echo 'Failed to complete upkeep!'"
