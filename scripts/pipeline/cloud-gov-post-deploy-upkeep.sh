7d286793d4bb85ca18a678edaf6987f4af59a050
tt-gsa <tyler.thompson@gsa.gov>
2024-02-16 13:07:01 -0500

    VOTE-846: update post deploy steps to handle prod ssh access

--- scripts/pipeline/cloud-gov-post-deploy-upkeep.sh
+++ scripts/pipeline/cloud-gov-post-deploy-upkeep.sh
@@ -1,2 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Enable SSH if in prod
+if [[ ${CIRCLE_BRANCH} = "prod" ]]; then
+  cf enable-ssh ${project}-drupal-${CIRCLE_BRANCH}
+  cf restart --strategy rolling ${project}-drupal-${CIRCLE_BRANCH}
+
+  # Wait until drupal app is running
+  until cf app ${project}-drupal-${CIRCLE_BRANCH} | grep running
+  do
+    sleep 1
+  done
+
+fi
+
 echo "Running upkeep..."
 cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/upkeep >/dev/null 2>&1 && echo 'Successfully completed upkeep!' || echo 'Failed to complete upkeep!'"
+
+## Clean up.
+if [[ ${CIRCLE_BRANCH} = "prod" ]]; then
+  cf disable-ssh ${project}-drupal-${CIRCLE_BRANCH}
+fi
