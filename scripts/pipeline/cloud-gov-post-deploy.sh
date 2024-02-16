570164f0fbfaacfe1cb1626bf257e69efa805984
tt-gsa <tyler.thompson@gsa.gov>
2024-02-16 11:00:25 -0500

    VOTE 846 testing

--- scripts/pipeline/cloud-gov-post-deploy.sh
+++ scripts/pipeline/cloud-gov-post-deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Wait until drupal app is running
-until [ cf app ${project}-drupal-${CIRCLE_BRANCH} | grep running ]
+until cf app ${project}-drupal-${CIRCLE_BRANCH} | grep running
 do
   sleep 1
 done
@@ -18,6 +18,6 @@ echo "Running post deploy steps..."
 cf ssh ${project}-drupal-${CIRCLE_BRANCH} --command "PATH=/home/vcap/deps/1/bin:/home/vcap/deps/0/bin:/usr/local/bin:/usr/bin:/bin:/home/vcap/app/php/bin:/home/vcap/app/php/sbin:/home/vcap/app/php/bin:/home/vcap/app/vendor/drush/drush app/scripts/post-deploy >/dev/null 2>&1 && echo 'Successfully completed post deploy!' || echo 'Failed to complete post deploy!'"
 
 ## Clean up.
-if [ ${BACKUP_ENV} = "prod" ]; then
+if [ ${BACKUP_ENV} = "test" ]; then
   cf disable-ssh ${project}-drupal-${BACKUP_ENV}
 fi
