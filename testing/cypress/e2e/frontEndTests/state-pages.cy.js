98035b105ef80eabe5550935be7631917493b4f9
christian medders <christian.medders@gsa.gov>
2024-04-15 10:18:30 -0400

    Vote-1056: Update to cypress tests data test ids

--- testing/cypress/e2e/frontEndTests/state-pages.cy.js
+++ testing/cypress/e2e/frontEndTests/state-pages.cy.js
@@ -5,23 +5,23 @@
 describe('test state pages', () => {
   it('test - online', () => {
     cy.visit('/register/ak')
-    cy.get('[data-test="registration-info"]').should('be.visible').and('contain', 'Online registration deadline:')
+    cy.get('[data-test="registrationInfo"]').should('be.visible').and('contain', 'Online registration deadline:')
   })
 
   it('test - in person', () => {
     cy.visit('/register/as')
-    cy.get('[data-test="registration-info"]').should('be.visible').and('contain', 'In person registration deadline:')
+    cy.get('[data-test="registrationInfo"]').should('be.visible').and('contain', 'In person registration deadline:')
   })
 
   it('test - mail in', () => {
     cy.visit('/register/ar')
-    cy.get('[data-test="registration-info"]').should('be.visible').and('contain', 'Register by mail deadline:')
-    cy.get('[data-test="nvrf-form"]').should('be.visible')
+    cy.get('[data-test="registrationInfo"]').should('be.visible').and('contain', 'Register by mail deadline:')
+    cy.get('[data-test="nvrfForm"]').should('be.visible')
   })
 
   it('test - not needed', () => {
     cy.visit('/register/nd')
-    cy.get('[data-test="registration-info"]').should('not.exist')
+    cy.get('[data-test="registrationInfo"]').should('not.exist')
   })
 
 })
