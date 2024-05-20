e11be9855f42708d44307423e43fd54b25abec29
christian medders <christian.medders@gsa.gov>
2024-05-20 12:37:20 -0400

    adding comments to script file

--- web/themes/custom/votegov/src/js/theme-switcher.js
+++ web/themes/custom/votegov/src/js/theme-switcher.js
@@ -9,21 +9,19 @@
   let currentIndex = 0;
 
     function toggleTheme() {
-
+      // once function is called move to the next item in themes array and then loop back at the end of the list
       currentIndex = (currentIndex + 1) % themes.length;
       container.setAttribute("data-theme", themes[currentIndex]);
-
+      // set theme selection in local storage
       localStorage.setItem('theme', themes[currentIndex]);
-
     }
 
     const storedTheme = localStorage.getItem('theme');
-
-if (storedTheme) {
-    // Set the current theme index based on the stored value
-    currentIndex = themes.indexOf(storedTheme);
-    container.setAttribute("data-theme", themes[currentIndex]);
-}
+    if (storedTheme) {
+        // Set the current theme index based on the stored value
+        currentIndex = themes.indexOf(storedTheme);
+        container.setAttribute("data-theme", themes[currentIndex]);
+    }
     
   contrastBtn.addEventListener('click', toggleTheme);
 })();
