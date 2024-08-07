6a34d4b9af6587b7723441a2ffeb783ed562ac67
christian medders <christian.medders@gsa.gov>
2024-08-07 12:54:41 -0400

    Update to external linlks test

--- testing/cypress/e2e/externalLinks/external-links-validator.cy.js
+++ testing/cypress/e2e/externalLinks/external-links-validator.cy.js
@@ -9,35 +9,6 @@ const excludedlinks = [
 ];
 
 describe('Validate External Links', () => {
-  // it('visit page', () => {
-  //     cy.visit('/sitemap')
-
-  //     cy.get('[class="vote-type-page node--page node--view-mode-full"] a').each(($link) => {
-  //       cy.visit($link.attr('href'))
-  //       // Get all links on the new page
-  //       cy.get("a[href^='https://']").each(link => {
-  //         if (excludedlinks.indexOf(link.prop('href')) == -1) {
-  //         cy.request({
-  //           url: link.prop('href'),
-  //           failOnStatusCode: false,
-  //         }).then((response) => {
-  //           if (response.status === 200) {
-  //             expect(response.status).to.eq(200)
-  //           } else if (response.status === 403){
-  //             cy.get('body').children().its('length').should('be.gt', 0)
-  //           } else if (response.status === 503){
-  //             throw new Error("site down - gave a 503")
-  //           } else if (response.status === 404){
-  //             throw new Error("page not found - gave a 404")
-  //           } else {
-  //             cy.get('body').children().its('length').should('be.gt', 0)
-  //           }
-  //         })
-  //       }
-  //     })
-  //     })
-  // })
-
   const urls = ['/sitemap', '/es/mapa-del-sitio'];
 
 urls.forEach((url) => {
