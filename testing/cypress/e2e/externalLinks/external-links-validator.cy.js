c4dc854745fea6de79b58185ff46f29b73d5ccc2
christian medders <christian.medders@gsa.gov>
2024-08-07 09:58:29 -0400

    Udpate to external links test

--- testing/cypress/e2e/externalLinks/external-links-validator.cy.js
+++ testing/cypress/e2e/externalLinks/external-links-validator.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-const allPages = require("../../fixtures/site-pages.json");
+// const allPages = require("../../fixtures/site-pages.json");
 
 const excludedlinks = [
   'https://voterregistration.ct.gov/OLVR/welcome.do?ref=voteusa_es',
@@ -8,53 +8,66 @@ const excludedlinks = [
     // the above link will throw error code "read ECONNRESET" this will not pass through cypress test and has been checked manually
 ];
 
-describe("External Link Validator Test", () => {
-  const singlePage =
-    Cypress.env("name") && Cypress.env("route")
-      ? [
-          {
-            name: Cypress.env("name"),
-            route: Cypress.env("route"),
-          },
-        ]
-      : null;
-  const pages = singlePage !== null ? singlePage : allPages;
-  pages.forEach((page) => {
-    it( 
-      `${page.name === "" ? "home" : page.name}`,
-      () =>
-        Cypress.env("retries") === true
-          ? {
-              retries: {
-                runMode: 2,
-              },
-            }
-          : {},
-      () => {
-        cy.visit({
-          url: page.route,
-        });
-        cy.get("a[href^='https://']").each(link => {
-          if (excludedlinks.indexOf(link.prop('href')) == -1) {
+describe('Validate External Links', () => {
+  // it('visit page', () => {
+  //     cy.visit('/sitemap')
+
+  //     cy.get('[class="vote-type-page node--page node--view-mode-full"] a').each(($link) => {
+  //       cy.visit($link.attr('href'))
+  //       // Get all links on the new page
+  //       cy.get("a[href^='https://']").each(link => {
+  //         if (excludedlinks.indexOf(link.prop('href')) == -1) {
+  //         cy.request({
+  //           url: link.prop('href'),
+  //           failOnStatusCode: false,
+  //         }).then((response) => {
+  //           if (response.status === 200) {
+  //             expect(response.status).to.eq(200)
+  //           } else if (response.status === 403){
+  //             cy.get('body').children().its('length').should('be.gt', 0)
+  //           } else if (response.status === 503){
+  //             throw new Error("site down - gave a 503")
+  //           } else if (response.status === 404){
+  //             throw new Error("page not found - gave a 404")
+  //           } else {
+  //             cy.get('body').children().its('length').should('be.gt', 0)
+  //           }
+  //         })
+  //       }
+  //     })
+  //     })
+  // })
+
+  const urls = ['/sitemap', '/es/mapa-del-sitio'];
+
+urls.forEach((url) => {
+  it(`visit page ${url}`, () => {
+    cy.visit(url);
+
+    cy.get('[class="vote-type-page node--page node--view-mode-full"] a').each(($link) => {
+      cy.visit($link.attr('href'));
+      // Get all links on the new page
+      cy.get("a[href^='https://']").each((link) => {
+        if (excludedlinks.indexOf(link.prop('href')) == -1) {
           cy.request({
             url: link.prop('href'),
             failOnStatusCode: false,
           }).then((response) => {
             if (response.status === 200) {
-              expect(response.status).to.eq(200)
-            } else if (response.status === 403){
-              cy.get('body').children().its('length').should('be.gt', 0)
-            } else if (response.status === 503){
-              throw new Error("site down - gave a 503")
-            } else if (response.status === 404){
-              throw new Error("page not found - gave a 404")
+              expect(response.status).to.eq(200);
+            } else if (response.status === 403) {
+              cy.get('body').children().its('length').should('be.gt', 0);
+            } else if (response.status === 503) {
+              throw new Error("site down - gave a 503");
+            } else if (response.status === 404) {
+              throw new Error("page not found - gave a 404");
             } else {
-              cy.get('body').children().its('length').should('be.gt', 0)
+              cy.get('body').children().its('length').should('be.gt', 0);
             }
-          })
+          });
         }
-      })
-      }
-    );
-  });    
+      });
+    });
+  });
 });
+})
