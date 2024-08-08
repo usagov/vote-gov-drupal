2cacec190a403c2f473d8b9ec7864d25bf5b6c62
mlloydbixal <amanda.lloyd@gsa.gov>
2024-08-08 15:37:52 -0400

    fix phplint errors

--- web/modules/custom/nvrf_app/src/Plugin/Block/nvrfapp.php
+++ web/modules/custom/nvrf_app/src/Plugin/Block/nvrfapp.php
@@ -1,8 +1,8 @@
 <?php
 namespace Drupal\nvrf_app\Plugin\Block;
 
-use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
@@ -16,29 +16,27 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 /**
  * Provides a block with NVRF react app's root id.
  */
-
 #[Block(
   id: "nvrf_app",
   admin_label: new TranslatableMarkup("NVRF Digital Form"),
   category: new TranslatableMarkup("Custom module")
 )]
 
-class nvrfapp extends BlockBase {
-/**
- * {@inheritdoc}
- */
-public function build() {
-  $build = [];
-  $build['custom_plugin'] = [
-  '#markup' => '<div id="root" class="nvrf-app-container"></div>',
-  '#attached' => [
-    'library' => [
-      'nvrf_app/nvrf_assets'
-    ]
-  ]
-    ];
-  return $build;
+class NVRFapp extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['custom_plugin'] = [
+      '#markup' => '<div id="root" class="nvrf-app-container"></div>',
+      '#attached' => [
+        'library' => [
+          'nvrf_app/nvrf_assets'
+        ]
+      ]
+      ];
+    return $build;
+  }
 
 }
-}
-
