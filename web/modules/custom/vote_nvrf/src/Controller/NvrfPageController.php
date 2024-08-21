8e807015443c823c5bdba13949b5d8b5e9cbbcda
Ray Estrada <raymond.estrada@gsa.gov>
2024-08-21 16:07:22 -0700

    VOTE-2357 initialize a custom nvrf page route (#934)

---
+++ web/modules/custom/vote_nvrf/src/Controller/NvrfPageController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\vote_nvrf\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * An NVRF page controller.
+ */
+class NvrfPageController extends ControllerBase {
+
+  /**
+   * Returns a renderable array for the nvrf app.
+   */
+  public function content(string $state_name) {
+    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $enabled_languages = ['en'];
+
+    // Return a 404 response if language is disabled.
+    if (!in_array($language, $enabled_languages)) {
+      throw new NotFoundHttpException();
+    }
+
+    // Transform the state name string for comparison.
+    $title = str_replace('-', ' ', $state_name);
+    // Query the nodes to find a match.
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+    $node = $node_storage->loadByProperties([
+      'type' => 'state_territory',
+      'status' => 1,
+      'field_accepts_nvrf' => TRUE,
+      'title' => $title,
+    ]);
+
+    if (empty($node)) {
+      // Return a 404 response if no matches are found.
+      throw new NotFoundHttpException();
+    }
+    else {
+      // Get the state abbreviation.
+      $node = reset($node);
+      $abbrev = $node->get('field_state_abbreviation')->value;
+
+      // Render the app in a custom page if a match was found.
+      $build = [
+        '#type' => 'inline_template',
+        '#template' => '<div id="root" data-stateId="{{ abbrev }}" class="nvrf-app-container vote-block-margin-y"></div>',
+        '#context' => [
+          'abbrev' => $abbrev,
+        ],
+        '#attached' => [
+          'library' => [
+            'vote_nvrf/nvrf_assets',
+          ],
+        ],
+      ];
+    }
+
+    return $build;
+  }
+
+}
