60e3458848a3fb148537e685adf5d30cf7bf64b4
Ray Estrada <raymond.estrada@gsa.gov>
2024-09-10 07:55:36 -0700

    Revert "Sprint 67 release candidate to STAGE (#966)"
    This reverts commit 65c54188ab798f889d0566b062a1f8f4e96535b5.

--- web/modules/custom/vote_nvrf/src/Controller/NvrfPageController.php
+++ web/modules/custom/vote_nvrf/src/Controller/NvrfPageController.php
@@ -15,7 +15,7 @@ class NvrfPageController extends ControllerBase {
    */
   public function content(string $state_name) {
     $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $enabled_languages = ['en', 'es'];
+    $enabled_languages = ['en'];
 
     // Return a 404 response if language is disabled.
     if (!in_array($language, $enabled_languages)) {
@@ -41,14 +41,12 @@ class NvrfPageController extends ControllerBase {
       // Get the state abbreviation.
       $node = reset($node);
       $abbrev = $node->get('field_state_abbreviation')->value;
-      $return_path = $node->getTranslation($language)->toUrl()->toString();
+      $return_path = $node->toUrl()->toString();
 
       // Render the app in a custom page if a match was found.
       $build = [
         '#type' => 'inline_template',
-        '#template' => '<div id="root" data-returnPath="{{ return_path }}" data-stateId="{{ abbrev }}" class="nvrf-app-container vote-block-margin-y--narrow">
-          <img src="/themes/custom/votegov/dist/assets/img/loader.svg" width="80px" width="80px" class="display-block margin-x-auto" />
-          </div>',
+        '#template' => '<div id="root" data-returnPath="{{ return_path }}" data-stateId="{{ abbrev }}" class="nvrf-app-container vote-block-margin-y--narrow"></div>',
         '#context' => [
           'abbrev' => $abbrev,
           'return_path' => $return_path,
