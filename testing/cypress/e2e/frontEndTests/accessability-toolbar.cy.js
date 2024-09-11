b2531ca2d4d7be95d0e3aad7459ceab459582fde
christian medders <christian.medders@gsa.gov>
2024-09-11 16:05:18 -0400

    adding page objects files and udpate tests

--- testing/cypress/e2e/frontEndTests/accessability-toolbar.cy.js
+++ testing/cypress/e2e/frontEndTests/accessability-toolbar.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import { pageObjects } from '../../support/pageObjects.js'
 
 describe('check accessability tool bar', () => {
   beforeEach('visit site', () => {
@@ -6,37 +7,62 @@ describe('check accessability tool bar', () => {
   })
 
   it('verify that tool bar is present', () => {
-    cy.get('[data-test="themeSwitcher"]').should('be.visible')
+    pageObjects
+    .themeSwitcher()
+    .should('be.visible')
   })
 
   it('verify page switches themes', () => {
-    cy.get('[data-test="themeContrast"]').click()
+    pageObjects
+    .themeContrast()
+    .click()
     cy.get('[data-theme="contrast"]').should('exist')
-    cy.get('[data-test="themeContrast"]').click()
+    
+    pageObjects
+    .themeContrast()
+    .click()
     cy.get('[data-theme="default"]').should('exist')
   })
 
   it('verify text enlarges', () => {
-    cy.get('[data-test="themeText"]').click()
+    pageObjects
+    .themeText()
+    .click()
     cy.get('[data-scale="large"]').should('exist')
-    cy.get('[data-test="themeText"]').click()
+
+    pageObjects
+    .themeText()
+    .click()
     cy.get('[data-scale="x-large"]').should('exist')
-    cy.get('[data-test="themeText"]').click()
+
+    pageObjects
+    .themeText()
+    .click()
     cy.get('[data-scale="default"]').should('exist')
   
   })
 
-  it.only('verify theme presents across pages', () => {
+  it('verify theme presents across pages', () => {
     // verify contrast theme 
-    cy.get('[data-test="themeContrast"]').click()
+    pageObjects
+    .themeContrast()
+    .click()
     cy.visit('/about-us').get('[data-theme="contrast"]').should('exist')
-    cy.get('[data-test="themeContrast"]').click()
+    
+    pageObjects
+    .themeContrast()
+    .click()
     cy.visit('/').get('[data-theme="default"]').should('exist')
 
     // verify text enlargement
-    cy.get('[data-test="themeText"]').click()
+    pageObjects
+    .themeText()
+    .click()
     cy.visit('/about-us').get('[data-scale="large"]').should('exist')
-    cy.get('[data-test="themeText"]').click()
+
+    pageObjects
+    .themeText()
+    .click()
     cy.visit('/').get('[data-scale="x-large"]').should('exist')
   })
 
--- testing/cypress/e2e/frontEndTests/footer-contact.cy.js
+++ testing/cypress/e2e/frontEndTests/footer-contact.cy.js
@@ -1,11 +1,15 @@
 // <reference types="cypress" />
 
+import { pageObjects } from "../../support/pageObjects"
+
 describe('Check Homepage for vote.gov', () =>{
 
   it('Test Footer Contact on Home Page', () => {
     // check that the footer contact section is present on home page
     cy.visit('/')
-    cy.get('[data-test="footerContact"]').should('be.visible')
-  })
 
+    pageObjects
+    .footerContact()
+    .should('be.visible')
+  })
 })
