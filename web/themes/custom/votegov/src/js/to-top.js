22667338c5aeb1a264a63a37bb02cc094decc2f9
mlloydbixal <amanda.lloyd@gsa.gov>
2024-09-19 23:06:51 -0400

    add throttle to function

--- web/themes/custom/votegov/src/js/to-top.js
+++ web/themes/custom/votegov/src/js/to-top.js
@@ -1,26 +1,30 @@
 const btt = document.querySelector('.back-to-top');
 
 let lastKnownScrollPosition = 0;
-let ticking = false;
 
-function doSomething(scrollComp) {
+function doSomething() {
+  scrollCompare = window.scrollY - lastKnownScrollPosition;
+  lastKnownScrollPosition = window.scrollY;
+  scrollComp = scrollCompare < 0 && window.scrollY > 60;
+
   if (scrollComp) {
-    btt.classList.add('js-scrolled');
+      btt.classList.add('js-scrolled');
   } else {
     btt.classList.remove('js-scrolled');
   }
 }
 
-document.addEventListener('scroll', function(e) {
-  scrollCompare = window.scrollY - lastKnownScrollPosition;
-  lastKnownScrollPosition = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(scrollCompare < 0 && window.scrollY > 60);
-      ticking = false;
-    });
-
-    ticking = true;
+const throttle = (func, timeFrame) => {
+  let lastTime = 0;
+  return function () {
+    const now = new Date();
+    if (now - lastTime >= timeFrame) {
+      func();
+      lastTime = now;
+    }
   }
-});
+}
+
+const throttledFunction = throttle(doSomething, 500);
+
+document.addEventListener('scroll', throttledFunction);
