61740cbbf776825f31d35fcdfe083a91e7396afb
christian medders <christian.medders@gsa.gov>
2024-11-01 11:26:04 -0400

    testing new link checking test

--- testing/cypress/e2e/externalLinks/external-links-validator.cy.js
+++ testing/cypress/e2e/externalLinks/external-links-validator.cy.js
@@ -37,6 +37,7 @@ describe("External Link Validator Test", () => {
             cy.request({
               url: linkUrl,
               failOnStatusCode: false,
+              timeout: 180000 // 3 minutes
             }).then((response) => {
               if (response.status === 200) {
                 expect(response.status).to.eq(200);
