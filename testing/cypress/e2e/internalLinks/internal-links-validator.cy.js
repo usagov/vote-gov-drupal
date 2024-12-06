0a3eca6e0842a42e97e96950198eea102463c5f0
christian medders <christian.medders@gsa.gov>
2024-12-06 14:04:47 -0500

    Vote-3159: Test Updates

--- testing/cypress/e2e/internalLinks/internal-links-validator.cy.js
+++ testing/cypress/e2e/internalLinks/internal-links-validator.cy.js
@@ -2,9 +2,9 @@
 
 const allPages = require("../../fixtures/site-pages.json");
 // ! IMPORTANT: When testing locally, comment out the LIVE_URL and use the LOCAL_URL listed below for testing purposes. This is necessary because our pipeline testing targets the live site. Remember to revert this change before committing, ensuring the LIVE_URL is set as the base.
-// const baseUrl = "http://vote-gov.lndo.site/";
+const baseUrl = "http://vote-gov.lndo.site/";
 
-const baseUrl = "https://vote.gov";
+// const baseUrl = "https://vote.gov";
 const excludedLinks = [
   'http://vote-gov.lndo.site/',
   'https://vote.gov'
@@ -13,20 +13,20 @@ const excludedLinks = [
 describe("Internal Link Validator Test", () => {
   const singlePage =
     Cypress.env("name") && Cypress.env("route")
-      ? [
+     ? [
           {
             name: Cypress.env("name"),
             route: Cypress.env("route"),
           },
         ]
       : null;
-  const pages = singlePage !== null ? singlePage : allPages;
+  const pages = singlePage!== null? singlePage : allPages;
   pages.forEach((page) => {
     it(
-      `${page.name === "" ? "home" : page.name}`,
+      `${page.name === ""? "home" : page.name}`,
       () =>
         Cypress.env("retries") === true
-          ? {
+         ? {
               retries: {
                 runMode: 2,
               },
@@ -34,18 +34,21 @@ describe("Internal Link Validator Test", () => {
           : {},
       () => {
         cy.visit(`${baseUrl}${page.route}`);
-          cy.get("a[href^='/']").each(link => {
+        cy.get("a[href^='/']").each((link) => {
+          if (
+           !link.parents(".usa-logo").length > 0 &&
+            excludedLinks.indexOf(link.prop("href")) === -1
+          ) {
             const href = link.prop("href");
-            if (excludedLinks.indexOf(href) === -1) {
             expect(href).not.match(/\/$/);
             cy.request({
-              url: link.prop('href'),
-              failOnStatusCode: false
+              url: link.prop("href"),
+              failOnStatusCode: false,
             }).then((response) => {
-              expect(response.status).to.eq(200)
-            })
+              expect(response.status).to.eq(200);
+            });
           }
-        })
+        });
       }
     );
   });
