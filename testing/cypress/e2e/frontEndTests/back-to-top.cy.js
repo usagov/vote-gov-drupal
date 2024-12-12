0dfbf27503754b8d17fca9eff12d1098c7008b1e
christian medders <christian.medders@gsa.gov>
2024-12-12 09:12:45 -0500

    Vote-3148: add tests for more coverage

---
+++ testing/cypress/e2e/frontEndTests/back-to-top.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+import { pageObjects } from "../../support/pageObjects"
+
+describe('check email sign up function', () => {
+  it('check that test is present once typed', () => {
+    cy.visit('/')
+
+    pageObjects
+
+  })
+})
