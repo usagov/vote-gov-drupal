07aec3bf4c784b331f904bde3d632e59ec0ac3b4
SamiraMSadat <samira.sadat@gsa.gov>
2024-12-17 13:22:16 -0700

    VOTE-3160 move js to file and attach as library

---
+++ web/themes/custom/votegov/src/js/touchpoints-click.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("vote-touchpoints-mobile--button");
+
+button.addEventListener("click", function() {
+  document.getElementById('vote-touchpoints--button').click();
+});
