f7935baf9b86b7610a5296438cd899e0fa03a867
christian medders <christian.medders@gsa.gov>
2024-12-20 09:42:15 -0500

    Vote-3190: Update to Touchpoints Test

--- testing/cypress/e2e/frontEndTests/touchpoints-module.cy.js
+++ testing/cypress/e2e/frontEndTests/touchpoints-module.cy.js
@@ -3,11 +3,29 @@
 import { pageObjects } from "../../support/pageObjects"
 
 describe('Check Touchpoints Module', () => {
-  it('Verify that module is opening', () => {
+  it('Verify that module is opening on desktop', () => {
     cy.visit('/')
 
     pageObjects
-    .touchpointsBtn().click()
+      .touchpointsBtn().then((btn) => {
+        cy.get(btn[1]).click()
+      })
+
+    cy.get('[class="fba-usa-modal fba-modal"]').should('be.visible')
+  })
+
+  it('Verify that module is opening on mobile', () => {
+    cy.visit('/')
+
+    cy.viewport('iphone-6')
+
+    pageObjects
+      .mobileBtn().click()
+
+    pageObjects
+      .touchpointsBtn().then((btn) => {
+        cy.get(btn[0]).click()
+      })
 
     cy.get('[class="fba-usa-modal fba-modal"]').should('be.visible')
   })
